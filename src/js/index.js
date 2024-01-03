e7e0c5b88f7cc7e013553354ac35aa6e5b56a9cd
Shant <shant212205@gmail.com>
2024-01-03 04:09:07 -0800

    update

--- src/js/index.js
+++ src/js/index.js
@@ -18,82 +18,122 @@ if (containerCreators || contentCreators) {
 	} else {
 		contentCreators.append(btnCreators);
 	}
+	window.matchMedia("(min-width:576px)").addEventListener("change", (e) => {
+		if (!e.matches) {
+			containerCreators.append(btnCreators)
+		} else {
+			contentCreators.append(btnCreators);
+		}
+	})
 }
 
-
-
-window.matchMedia("(min-width:576px)").addEventListener("change", (e) => {
-	if (!e.matches) {
-		containerCreators.append(btnCreators)
-	} else {
-		contentCreators.append(btnCreators);
-	}
-})
 
 const btnDiscover = document.querySelector(".discover__btn");
 const containerDiscover = document.querySelector(".discover__container");
 const contentDiscover = document.querySelector(".discover__content");
 
-
-if (!window.matchMedia("(min-width:768px)").matches) {
-	containerDiscover.append(btnDiscover);
-} else {
-	contentDiscover.append(btnDiscover);
-}
-
-
-
-window.matchMedia("(min-width:768px)").addEventListener("change", (e) => {
-	if (!e.matches) {
-		containerDiscover.append(btnDiscover)
+if (containerDiscover || contentDiscover) {
+	if (!window.matchMedia("(min-width:768px)").matches) {
+		containerDiscover.append(btnDiscover);
 	} else {
 		contentDiscover.append(btnDiscover);
 	}
-})
+	window.matchMedia("(min-width:768px)").addEventListener("change", (e) => {
+		if (!e.matches) {
+			containerDiscover.append(btnDiscover)
+		} else {
+			contentDiscover.append(btnDiscover);
+		}
+	})
+}
+
 
 const btnMagic = document.querySelector(".magic__btn");
 const containerMagic = document.querySelector(".magic__container");
 const contentMagic = document.querySelector(".magic__content");
 
+if (containerMagic || contentMagic) {
+	if (!window.matchMedia("(min-width:768px)").matches) {
+		containerMagic.append(btnMagic);
+	} else {
+		contentMagic.append(btnMagic);
+	}
+	window.matchMedia("(min-width:768px)").addEventListener("change", (e) => {
+		if (!e.matches) {
+			containerMagic.append(btnMagic)
+		} else {
+			contentMagic.append(btnMagic);
+		}
+	})
+}
 
-if (!window.matchMedia("(min-width:768px)").matches) {
-	containerMagic.append(btnMagic);
-} else {
-	contentMagic.append(btnMagic);
+const btnArtist = document.querySelector("[data-btn]");
+const containerArtist = document.querySelector("[date-container]");
+const contentArtist = document.querySelector("[date-box]");
+
+if (containerArtist || contentArtist) {
+	if (!window.matchMedia("(min-width:768px)").matches) {
+		containerArtist.append(btnArtist);
+	} else {
+		contentArtist.append(btnArtist);
+	}
+	window.matchMedia("(min-width:768px)").addEventListener("change", (e) => {
+		if (!e.matches) {
+			containerArtist.append(btnArtist)
+		} else {
+			contentArtist.append(btnArtist);
+		}
+	})
 }
 
 
 
-window.matchMedia("(min-width:768px)").addEventListener("change", (e) => {
-	if (!e.matches) {
-		containerMagic.append(btnMagic)
-	} else {
-		contentMagic.append(btnMagic);
-	}
-})
 
 const hours = document.querySelector(".magic__hours");
 const minuts = document.querySelector(".magic__minutes");
 const seconds = document.querySelector(".magic__seconds");
+if (hours || minuts || seconds) {
+	setInterval(() => {
+		let date = new Date();
+		let h = date.getHours();
+		let m = date.getMinutes();
+		let s = date.getSeconds();
 
-setInterval(() => {
-	let date = new Date();
-	let h = date.getHours();
-	let m = date.getMinutes();
-	let s = date.getSeconds();
+		if (h > 12) {
+			h = h - 12
+		}
 
-	if (h > 12) {
-		h = h - 12
-	}
+		// h = h<10 ? "0"+h : h;
+		// m = h<10 ? "0"+m : m;
+		// s = h<10 ? "0"+s : s;
 
-	// h = h<10 ? "0"+h : h;
-	// m = h<10 ? "0"+m : m;
-	// s = h<10 ? "0"+s : s;
+		hours.innerHTML = h;
+		minuts.innerHTML = m;
+		seconds.innerHTML = s;
+	})
+}
 
-	hours.innerHTML = h;
-	minuts.innerHTML = m;
-	seconds.innerHTML = s;
-})
+const heroHours = document.querySelector("[date-hours]");
+const heroMinuts = document.querySelector("[date-minutes]");
+const heroSeconds = document.querySelector("[date-seconds]");
+if (heroHours || heroMinuts || heroSeconds) {
+	setInterval(() => {
+		let date = new Date();
+		let h = date.getHours();
+		let m = date.getMinutes();
+		let s = date.getSeconds();
 
+		if (h > 12) {
+			h = h - 12
+		}
 
+		// h = h<10 ? "0"+h : h;
+		// m = h<10 ? "0"+m : m;
+		// s = h<10 ? "0"+s : s;
+
+		heroHours.innerHTML = h;
+		heroMinuts.innerHTML = m;
+		heroSeconds.innerHTML = s;
+	})
+}
 
